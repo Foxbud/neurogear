7bce4ac9d53e98d3f563a695427ce5e62f5ac705
garrett <garrett@GT-Xubuntu>
2017-04-07 13:22:42 -0400

    Added code for regularization, but it has not been integrated yet.

---
+++ base/regularization/L1Regularization.java
@@ -0,0 +1,42 @@
+package neurogear.base.regularization;
+
+/**
+ * L1 implementation of Regularization.
+ * 
+ * @author Garrett Russell Fairburn
+ * @version 1.0
+ * File: L1Regularization.java
+ * Created: 04/07/17
+ * Copyright (c) 2017, Garrett Russell Fairburn, All rights reserved.
+ * Summary of Modifications:
+ *  N/A
+ * 
+ * Description: This is the implementation for L1
+ * regularization.
+ */
+public final class L1Regularization implements Regularization {
+    
+    /**
+     * Derivative of L1 regularization.
+     * @param regParameter regularization parameter
+     * @param weight weight value
+     * @return f'(x) = regParameter * sign(weight)
+     */
+    @Override
+    public double df(double regParameter, double weight) {
+    
+        // Return the regularization parameter times the sign of the weight value.
+        if (weight > 0.0) {
+        
+            return regParameter;
+        }
+        else if (weight < 0.0) {
+        
+            return -regParameter;
+        }
+        else {
+        
+            return 0.0;
+        }
+    }
+}
---
+++ base/regularization/Regularization.java
@@ -0,0 +1,26 @@
+package neurogear.base.regularization;
+
+/**
+ * Interface for implementing regularization.
+ * 
+ * @author Garrett Russell Fairburn
+ * @version 1.0
+ * File: Regularization.java
+ * Created: 04/07/17
+ * Copyright (c) 2017, Garrett Russell Fairburn, All rights reserved.
+ * Summary of Modifications:
+ *  N/A
+ * 
+ * Description: Implementations must specify df(), the derivative of 
+ * the regularization. Implementations should also be final.
+ */
+public interface Regularization {
+    
+    /**
+     * The derivative of the regularization.
+     * @param regParameter regularization parameter
+     * @param weight weight value
+     * @return f'(x)
+     */
+    public double df(double regParameter, double weight);
+}
