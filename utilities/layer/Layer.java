c3a38741e2817d15759ec9adcae0808f56e256d6
garrett <garrett@GT-Xubuntu>
2017-05-23 11:49:12 -0400

    Started implementing Layer class.

--- utilities/layer/Layer.java
+++ utilities/layer/Layer.java
@@ -1,5 +1,11 @@
 package neurogear.utilities.layer;
 
+import java.util.Arrays;
+import java.util.Random;
+import neurogear.base.activation.Activation;
+import neurogear.base.node.Node;
+import neurogear.utilities.kernel.Kernel;
+
 /**
  * Layer class for neural networks.
  * 
@@ -14,14 +20,14 @@ package neurogear.utilities.layer;
  * Description: Holds a group a similar Nodes
  * and their corresponding input Connections.
  */
-public class Layer {
+public final class Layer {
     
     /*
     Outline:
     
     Data:
     array of Kernels
-    array of Nodes
+    2d array of Nodes
     activation function
     input Layer
     has output Layer
@@ -35,10 +41,117 @@ public class Layer {
     disconnect
     hasOutput
     toggleOutput
-    getActivations
-    setSums(array of doubles)
+    expressNodes
+    getActivationValues
+    setActivationSums(array of doubles)
     propagate
     backpropagate
     correct(hyperparameters)
     */
+    
+    // MEMBER VARIABLES.
+    
+    // Kernel for each feature.
+    private final Kernel kernels[];
+    // Nodes for each feature.
+    private final Node nodes[][];
+    
+    // Activation function for Nodes.
+    private final Activation activationFunction;
+    
+    // This Layer's input Layer.
+    private Layer inputLayer;
+    // States whether this Layer is input to another layer.
+    private boolean hasOutputLayer;
+    
+    // MEMBER METHODS.
+    
+    public Layer(int numFeatures, int nodesPerFeature, int offsets[], int strideLength, Activation activationFunctionP, int seed) {
+    
+        this(numFeatures, nodesPerFeature, activationFunctionP);
+        
+        // Initialize PRNG for weight generation.
+        Random PRNG = new Random(seed);
+        
+        // Create weight arrays.
+        double weights[][] = new double[numFeatures][offsets.length + 1];
+        
+        // Generate weights.
+        for (int i = 0; i < weights.length; i++) {
+        
+            // Set NodeConnection weights.
+            for (int j = 0; j < weights[i].length - 1; j++) {
+            
+                weights[i][j] = PRNG.nextDouble() / Math.sqrt(weights[i].length - 1);
+            }
+            
+            // Set BiasConnection weight.
+            weights[i][weights[i].length - 1] = 0.0;
+        }
+        
+        // Initialize Kernel array.
+        for (int i = 0; i < kernels.length; i++) {
+        
+            kernels[i] = new Kernel(offsets, weights[i], strideLength);
+            
+            // Connect Kernel to appropriate output.
+            kernels[i].connectOutputNodes(nodes[i]);
+        }
+    }
+    
+    public Layer(int numFeatures, int nodesPerFeature, int numConnections, int strideLength, Activation activationFunctionP, int seed) {
+    
+        this(numFeatures, nodesPerFeature, activationFunctionP);
+        
+        // Initialize PRNG for weight generation.
+        Random PRNG = new Random(seed);
+        
+        // Create weight arrays.
+        double weights[][] = new double[numFeatures][numConnections + 1];
+        
+        // Generate weights.
+        for (int i = 0; i < weights.length; i++) {
+        
+            // Set NodeConnection weights.
+            for (int j = 0; j < weights[i].length - 1; j++) {
+            
+                weights[i][j] = PRNG.nextDouble() / Math.sqrt(weights[i].length - 1);
+            }
+            
+            // Set BiasConnection weight.
+            weights[i][weights[i].length - 1] = 0.0;
+        }
+        
+        // Initialize Kernel array.
+        for (int i = 0; i < kernels.length; i++) {
+        
+            kernels[i] = new Kernel(numConnections, weights[i], strideLength);
+            
+            // Connect Kernel to appropriate output.
+            kernels[i].connectOutputNodes(nodes[i]);
+        }
+    }
+    
+    // HELPER METHODS.
+    
+    private Layer(int numFeatures, int nodesPerFeature, Activation activationFunctionP) {
+    
+        // Create 2d Node array.
+        nodes = new Node[numFeatures][nodesPerFeature];
+        
+        // Initialize 2d Node array.
+        for (int i = 0; i < nodes.length; i++) {
+        
+            Arrays.fill(nodes[i], new Node());
+        }
+        
+        // Create Kernel array.
+        kernels = new Kernel[numFeatures];
+        
+        // Set activation function.
+        activationFunction = activationFunctionP;
+        
+        // Initialize hasOutputLayer.
+        hasOutputLayer = false;
+    }
 }
