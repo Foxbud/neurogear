41219b01bc86a0952ee1fc1e22589b42ae6f75b1
garrett <garrett@GT-Xubuntu>
2017-05-28 12:51:00 -0400

    Updated exceptions for Kernel class.

---
+++ utilities/layer/kernel/InvalidArrayException.java
@@ -0,0 +1,28 @@
+package neurogear.utilities.layer.kernel;
+
+/**
+ * Exception generated when a Kernel 
+ * receives and invalid array.
+ * 
+ * @author Garrett Russell Fairburn
+ * @version 1.0
+ * File: InvalidArrayException.java
+ * Created: 05/28/17
+ * Copyright (c) 2017, Garrett Russell Fairburn, All rights reserved.
+ * Summary of Modifications:
+ *  N/A
+ * 
+ * Description: Exception generated when 
+ * a Kernel receives and invalid array.
+ */
+public final class InvalidArrayException extends RuntimeException {
+    
+    /**
+     * Construct an exception with a given message.
+     * @param message exception message
+     */
+    public InvalidArrayException(String message) {
+    
+        super(message);
+    }
+}
--- utilities/layer/kernel/Kernel.java
+++ utilities/layer/kernel/Kernel.java
@@ -1,5 +1,6 @@
  package neurogear.utilities.layer.kernel;
 
+import java.util.Arrays;
 import neurogear.base.connection.BiasConnection;
 import neurogear.base.connection.NodeConnection;
 import neurogear.base.node.Node;
@@ -48,8 +49,28 @@ public final class Kernel {
      * @param numChannels number of input channels for this Kernel
      * @param receptiveFieldP each column of Connections' relative offset to the stride index
      * @param strideLengthP number of input Node columns to stride by
+     * @throws InvalidArrayException if parameter 'receptiveFieldP' is not valid
+     * @throws InvalidSizeException if the parameters 'numChannels' and 'strideLengthP' or not valid
      */
     public Kernel(int numChannels, int receptiveFieldP[], int strideLengthP) {
+        
+        // Test for exceptions.
+        if (receptiveFieldP == null) {
+        
+            throw new InvalidArrayException("'receptiveFieldP' must not be null");
+        }
+        else if (receptiveFieldP.length == 0) {
+        
+            throw new InvalidArrayException("'receptiveFieldsP' must not be empty");
+        }
+        else if (numChannels <= 0) {
+        
+            throw new InvalidSizeException("'numChannels' must be greater than zero");
+        }
+        else if (strideLengthP <= 0) {
+        
+            throw new InvalidSizeException("'strideLengthP' must be greater than zero");
+        }
         
         // Create connections array.
         primaryConnections = new NodeConnection[numChannels][receptiveFieldP.length];
@@ -76,8 +97,34 @@ public final class Kernel {
     /**
      * Connect a 2D array of Nodes to this Kernel's input.
      * @param inputNodesP Nodes to connect
+     * @throws InputOverrideException if Kernel already has input
+     * @throws InvalidArrayException if parameter 'inputNodesP' is not valid
+     * @throws SizeConflictException if parameter 'inputNodesP' and internal Connections have conflicting numbers of channels
+     * @throws ReceptiveFieldConflictException if input and output are not compatible with receptive field
      */
     public void connectInputNodes(Node inputNodesP[][]) {
+        
+        // Test for exceptions.
+        if (inputNodes != null) {
+        
+            throw new InputOverrideException("cannot set new input without disconnecting current input");
+        }
+        else if (inputNodesP == null) {
+        
+            throw new InvalidArrayException("'inputNodesP' must not be null");
+        }
+        else if (inputNodesP.length == 0) {
+        
+            throw new InvalidArrayException("'inputNodesP' must not be empty");
+        }
+        else if (inputNodes.length != primaryConnections.length) {
+        
+            throw new SizeConflictException("'inputNodesP' has " + inputNodesP.length + " channels while Kernel is configured for " + primaryConnections.length + " channels");
+        }
+        else if (outputNodes != null) {
+        
+            testForReceptiveFieldConflict(inputNodesP, outputNodes);
+        }
         
         inputNodes = inputNodesP.clone();
     }
@@ -85,8 +132,29 @@ public final class Kernel {
     /**
      * Connect an array of Nodes to this Kernel's output.
      * @param outputNodesP Nodes to connect
+     * @throws OutputOverrideException if Kernel already has output
+     * @throws InvalidArrayException if parameter 'outputNodesP' is not valid
+     * @throws ReceptiveFieldConflictException if input and output are not compatible with receptive field
      */
     public void connectOutputNodes(Node outputNodesP[]) {
+        
+        // Test for exceptions.
+        if (outputNodes != null) {
+        
+            throw new OutputOverrideException("cannot set new output without disconnecting current output");
+        }
+        else if (outputNodesP == null) {
+        
+            throw new InvalidArrayException("'outputNodesP' must not be null");
+        }
+        else if (outputNodesP.length == 0) {
+        
+            throw new InvalidArrayException("'outputNodesP' must not be empty");
+        }
+        else if (inputNodes != null) {
+        
+            testForReceptiveFieldConflict(inputNodes, outputNodesP);
+        }
         
         outputNodes = outputNodesP.clone();
     }
@@ -134,8 +202,20 @@ public final class Kernel {
      * Set the weights of this Kernel's
      * Connections (including bias).
      * @param weights weights to copy (the last of which is bias)
+     * @throws InvalidArrayException if parameter 'weights' is null
+     * @throws SizeConflictException if size of parameter 'weights' and number of Connections conflicts
      */
     public void setWeights(double weights[]) {
+        
+        // Test for exceptions.
+        if (weights == null) {
+        
+            throw new InvalidArrayException("'weights' must not be null");
+        }
+        else if (weights.length != primaryConnections.length * primaryConnections[0].length + 1) {
+        
+            throw new SizeConflictException("size of 'weights' and number of Connections conflict");
+        }
         
         // Set weight values.
         for (int channelI = 0; channelI < primaryConnections.length; channelI++) {
@@ -197,6 +277,50 @@ public final class Kernel {
     // HELPER METHODS.
     
     /**
+     * Test current input, output, and receptive
+     * field for incompatibilities.
+     * @param input input Nodes
+     * @param output output Nodes
+     * @throws ReceptiveFieldConflictException if input and output are not compatible with receptive field
+     */
+    private void testForReceptiveFieldConflict(Node input[][], Node output[]) {
+    
+        // Array of booleans to track which input columns have been visited.
+        boolean visited[] = new boolean[input[0].length];
+        Arrays.fill(visited, false);
+        
+        // Iterate through all output Nodes.
+        for (int oNodeI = 0; oNodeI < output.length; oNodeI++) {
+        
+            // True offset index.
+            int trueOffset = oNodeI * strideLength;
+            
+            // Iterate through all offsets in receptive field.
+            for (int rpI = 0; rpI < receptiveField.length; rpI++) {
+            
+                // Test for exception.
+                if (trueOffset + receptiveField[rpI] >= inputNodes[0].length) {
+                
+                    throw new ReceptiveFieldConflictException("receptive field column #" + rpI + " will fall out of bounds on stride #" + oNodeI);
+                }
+                
+                // Set appropriate visited boolean.
+                visited[trueOffset + receptiveField[rpI]] = true;
+            }
+        }
+        
+        // Check if all input columns were vistied.
+        for (int column = 0; column < visited.length; column++) {
+        
+            // Test for exception.
+            if (!visited[column]) {
+            
+                throw new ReceptiveFieldConflictException("input Node column #" + column + " will not be visited by receptive field");
+            }
+        }
+    }
+    
+    /**
      * Perform a single propagation stride.
      * @param strideNum current stride number
      */
@@ -242,7 +366,7 @@ public final class Kernel {
                 primaryConnections[channelI][nodeI].setInput(inputNodes[channelI][nodeI + strideNum * strideLength]);
                 primaryConnections[channelI][nodeI].setOutput(outputNodes[strideNum]);
                 
-                // Propagate Connection.
+                // Backpropagate Connection.
                 primaryConnections[channelI][nodeI].backpropagate();
                 
                 // Disconnect Connection.
